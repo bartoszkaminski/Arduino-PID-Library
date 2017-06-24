481ce3f324896b278583de5db86b94d3a5db3b37
Bartosz Kamiński <bartosz@kaminski.me>
2017-06-24 19:03:50 +0200

    Compute PID output every time Compute() is called

--- PID_v1.cpp
+++ PID_v1.cpp
@@ -58,10 +58,6 @@ PID::PID(double* Input, double* Output, double* Setpoint,
 bool PID::Compute()
 {
    if(!inAuto) return false;
-   unsigned long now = millis();
-   unsigned long timeChange = (now - lastTime);
-   if(timeChange>=SampleTime)
-   {
       /*Compute all the working error variables*/
       double input = *myInput;
       double error = *mySetpoint - input;
@@ -88,10 +84,7 @@ bool PID::Compute()
 
       /*Remember some variables for next time*/
       lastInput = input;
-      lastTime = now;
 	    return true;
-   }
-   else return false;
 }
 
 /* SetTunings(...)*************************************************************
